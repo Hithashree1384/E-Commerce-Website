aa2f7f3a61ce4d95a68fa5165112149674d865cf
Hithashree A M <120492322+Hithashree1384@users.noreply.github.com>
2025-09-12 22:54:50 +0530

    Update LoginSignup.jsx

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -15,7 +15,7 @@ const LoginSignup = () => {
   const login=async()=>{
     console.log("Login executed",formData);
     let responsedata;
-    await fetch('http://localhost:4000/login',{
+    await fetch('https://e-commerce-website-backend-f5sh.onrender.com/login',{
       method:'POST',
       headers:{
         Accept:'application/json',
@@ -35,7 +35,7 @@ const LoginSignup = () => {
   const signup=async()=>{
     console.log("Signup executed",formData);
     let responsedata;
-    await fetch('http://localhost:4000/signup',{
+    await fetch('https://e-commerce-website-backend-f5sh.onrender.com/signup',{
       method:'POST',
       headers:{
         Accept:'application/json',
