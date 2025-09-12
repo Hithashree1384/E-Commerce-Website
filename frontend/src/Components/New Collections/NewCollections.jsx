06cc43fe98b8ffc35db62edd925fbf12d6c417c4
Hithashree A M <120492322+Hithashree1384@users.noreply.github.com>
2025-09-12 23:52:35 +0530

    Update NewCollections.jsx

--- frontend/src/Components/New Collections/NewCollections.jsx
+++ frontend/src/Components/New Collections/NewCollections.jsx
@@ -6,7 +6,7 @@ import Item from '../Items/Item'
 const NewCollections = () => {
   const [new_collections,setNew_collections]=useState([])
 useEffect(() => {
-  fetch('https://e-commerce-website-backend-f5sh.onrender.com/newcollections')
+  fetch('https://e-commerce-website-backend-8nct.onrender.com/newcollections')
     .then((response) => response.json()) 
     .then((data) => setNew_collections(data))
     .catch((error) => console.error("Error fetching collections:", error));
